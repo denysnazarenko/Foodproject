7c245e67c4392c24fe0c43fa39fa4858359ad5f9
denys nazarenko <dinis.nazarenko03@gmail.com>
2024-02-01 11:14:15 +0200

    add fetch API

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
   
 
   // Timer
-  const deadline = '2023-12-02';
+  const deadline = '2024-04-04';
 
   function getTimeRemaining(endtime) {
     let days, hours, minutes, seconds;
@@ -244,10 +244,6 @@ window.addEventListener('DOMContentLoaded', () => {
       `;
       form.insertAdjacentElement('afterend', statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-      
-      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
       const object = {};
@@ -255,18 +251,22 @@ window.addEventListener('DOMContentLoaded', () => {
         object[key] = value;
       });
 
-      const json = JSON.stringify(object);
-      request.send(json);
-
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          console.log(request.response);
-          showThanksModal(message.success);
-          form.reset();
-          statusMessage.remove();
-        } else {
-          showThanksModal(message.failure);
-        }
+      fetch('server.php', {
+        method: "POST",
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(object)
+      })
+      .then(data => data.text())
+      .then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.failure);
+      }).finally(() => {
+        form.reset();
       });
     });
   }
@@ -294,4 +294,14 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
+
+  // fetch('https://jsonplaceholder.typicode.com/posts', {
+  //   method: "POST",
+  //   body: JSON.stringify({name: 'Alex'}),
+  //   headers: {
+  //     'Content-type': 'application/json'
+  //   }
+  // })
+  // .then(response => response.json())
+  // .then(json => console.log(json));
 });
