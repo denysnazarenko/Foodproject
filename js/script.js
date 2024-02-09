6536c8b2339b72813f5381fbebd3ed3f347e6387
denys nazarenko <dinis.nazarenko03@gmail.com>
2024-02-09 18:27:56 +0200

    add slider 2

--- js/script.js
+++ js/script.js
@@ -322,101 +322,115 @@ window.addEventListener('DOMContentLoaded', () => {
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
   let slideIndex = 1;
-
-  showSlides(slideIndex);
+  let offset = 0;
 
   if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
   } else {
     total.textContent = slides.length;
+    current.textContent = slideIndex;
   }
 
-  function showSlides(n) {
-    if (n > slides.length) {
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
+
+  slidesWrapper.style.overflow = 'hidden';
+
+  slides.forEach(slide => {
+    slide.style.width = width;
+  });
+
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+      offset = 0;
+    } else {
+      offset += +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform =  `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
       slideIndex = 1;
+    } else {
+      slideIndex++;
     }
 
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    slides.forEach(item => item.style.display = 'none');
-
-    slides[slideIndex - 1].style.display = 'block';
-
-    if (slides.length < 10) { // при умові slides.length < 10 трішки ломається, потрібно робити slideIndex < 10
+    if (slides.length < 10) {
       current.textContent = `0${slideIndex}`;
     } else {
       current.textContent = slideIndex;
     }
-  }
-
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+  });
 
   prev.addEventListener('click', () => {
-    plusSlides(-1);
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+      offset -= +width.slice(0, width.length - 2);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+      slideIndex = slides.length;
+    } else {
+      slideIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   });
 
-  next.addEventListener('click', () => {
-    plusSlides(1);
-  });
-  
 
+  // Простий варіант слайдера
+  // showSlides(slideIndex);
 
-  // зробив сам
-  // const sliderArrowParents = document.querySelector('.offer__slider-counter'),
-  //       sliderContent = document.querySelectorAll('.offer__slide'),
-  //       sliderCurrent = sliderArrowParents.querySelector('#current'),
-  //       sliderTotal = sliderArrowParents.querySelector('#total');
-  
-  // function hideSliderContent() {
-  //   sliderContent.forEach(slide => {
-  //     slide.classList.add('hide');
-  //     slide.classList.remove('show', 'fade');
-  //   });
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`;
+  // } else {
+  //   total.textContent = slides.length;
   // }
 
-  // function showSliderContent(i = 0) {
-  //   sliderContent[i].classList.add('show', 'fade');
-  //   sliderContent[i].classList.remove('hide');
-  //   sliderCurrent.textContent = `${getZero(i + 1)}`;
-  //   sliderTotal.textContent = `${getZero(sliderContent.length)}`;
-  // }
-
-  // hideSliderContent();
-  // showSliderContent();
-
-  // sliderArrowParents.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   const target = e.target;
-  //   let iteration;
-  //   sliderContent.forEach((slide, i) => {
-  //     if (slide.classList.contains('show')) {
-  //       return iteration = i;
-  //     }
-  //   });
-    
-  //   if (target && target.classList.contains('offer__slider-next')) {
-  //     if (iteration === sliderContent.length - 1) {
-  //       iteration = 0;
-  //     } else {
-  //       iteration++;
-  //     }
-  //     hideSliderContent();
-  //     showSliderContent(iteration);
-  //   } else if (target && target.classList.contains('offer__slider-prev')) {
-  //     if (iteration === 0) {
-  //       iteration = sliderContent.length - 1;
-  //     } else {
-  //       iteration--;
-  //     }
-  //     hideSliderContent();
-  //     showSliderContent(iteration);
+  // function showSlides(n) {
+  //   if (n > slides.length) {
+  //     slideIndex = 1;
   //   }
+
+  //   if (n < 1) {
+  //     slideIndex = slides.length;
+  //   }
+
+  //   slides.forEach(item => item.style.display = 'none');
+
+  //   slides[slideIndex - 1].style.display = 'block';
+
+  //   if (slides.length < 10) { // при умові slides.length < 10 трішки ломається, потрібно робити slideIndex < 10
+  //     current.textContent = `0${slideIndex}`;
+  //   } else {
+  //     current.textContent = slideIndex;
+  //   }
+  // }
+
+  // function plusSlides(n) {
+  //   showSlides(slideIndex += n);
+  // }
+
+  // prev.addEventListener('click', () => {
+  //   plusSlides(-1);
+  // });
+
+  // next.addEventListener('click', () => {
+  //   plusSlides(1);
   // });
 
 });
